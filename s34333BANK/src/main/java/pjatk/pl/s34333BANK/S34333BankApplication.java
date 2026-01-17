7ed50cd9296e0d41a126e5f5880347094736ba34
Joanna Jaroszewska <s34333@pjwstk.edu.pl>
2026-01-17 12:50:24 +0100

    Initial commit

---
+++ s34333BANK/src/main/java/pjatk/pl/s34333BANK/S34333BankApplication.java
@@ -0,0 +1,13 @@
+package pjatk.pl.s34333BANK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34333BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S34333BankApplication.class, args);
+	}
+
+}
